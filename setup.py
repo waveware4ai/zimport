0a6b4fd33aec3aa4c2e6820606776790a634393b
waveware4ai <50553398+waveware4ai@users.noreply.github.com>
2025-05-22 12:31:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
-from glob import glob
-from os.path import basename, splitext
 
 setup(
     name='zimport',
     version='0.1.0',
+    author='14mhz',
+    author_email='14mhz@hanmail.net',    
     description='zimport is used to load and manage python packages from zip-archives.',
+    keywords='python import package zip pyd',
     url='https://github.com/waveware4ai/zimport',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     license='GPL-3.0 license',
-    author='14mhz',
-    author_email='14mhz@hanmail.net',
-    keywords='python import package zip pyd',
     python_requires='>=3.8',   
     packages=['zimport','zimport.util'],
 )
+
