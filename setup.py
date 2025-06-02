91cdae7cf0487d4ef45618b884b8963c7cdd09a9
waveware4ai <50553398+waveware4ai@users.noreply.github.com>
2025-06-02 20:54:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zimport',
-    version='0.1.5',
+    version='0.1.6',
     author='14mhz',
     author_email='14mhz@hanmail.net',    
     description='zimport is used to load and manage python packages from zip-archives.',
